4d041532c8229fe71479389141bef59aa74f53c0
Rauf <rfhusv@gmail.com>
2023-01-28 01:33:29 +0400

    Complete exercise

--- javascript.js
+++ javascript.js
@@ -11,14 +11,27 @@ function playerChoice() {
 }
 
 function playRound (playerSelection, computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection);
-    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-        return alert("You win!");
-    } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
-        return alert("You lose!");
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return alert("You Won! Rock beats Scissors");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return alert("You Won! Paper beats Rock");
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return alert("You Won! Scissors beats Paper");
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return alert("You Lose! Paper beats Rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return alert("You Lose! Scissors beats Paper");
+    } else if (playerSelection == "scissors" && computerSelection === "rock") {
+        return alert("You Lose! Rock beats Scissors");
     } else {
-        return alert("Tie!");
+        alert("Tie!")
     }
 }
-console.log(playRound(playerChoice(), getComputerChoice()));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerChoice(), getComputerChoice());
+    }
+}
+game();
